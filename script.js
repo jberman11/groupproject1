ec6b2ad35ecf9154f62488d702e0bed5cb5bf71c
Shuler <msmehaf2@gmail.com>
2018-11-29 18:53:57 -0600

    created stylesheets for each school. Stylesheets change on search. Moved stylechanges to in the buttonclick function

--- script.js
+++ script.js
@@ -8,9 +8,96 @@ function getWeather() {
 
 $("#searchButton").on("click", function() {
     event.preventDefault()
+    var school = $("#userInput").val().trim();
     $(".card").css("display","initial");
     $("#teamLocals").hide();
     getWeather();
+
+    if (school === "austin") {
+        $("#stylesheet1").attr("href", "./assets/stylesheets/uta.css");
+        var tLogo = $("<img>");
+        tLogo.attr("src", "assets/images/texas_longhorns.png");
+        tLogo.attr("alt", "The University of Texas at Austin Team Logo");
+        $("#teamLogo").html(tLogo);
+        $("#emess").remove();
+    } else if (school === "waco") {
+        $("#stylesheet1").attr("href", "./assets/stylesheets/baylor.css");
+        var tLogo = $("<img>");
+        tLogo.attr("src", "assets/images/baylor_bears.png");
+        tLogo.attr("alt", "University of Baylor Team Logo");
+        $("#teamLogo").html(tLogo);
+        $("#emess").remove();
+    } else if (school === "ames") {
+        $("#stylesheet1").attr("href", "./assets/stylesheets/isu.css");
+        var tLogo = $("<img>");
+        tLogo.attr("src", "assets/images/iowa_state_cyclones.png");
+        tLogo.attr("alt", "Iowa State University Team Logo");
+        $("#teamLogo").html(tLogo);
+        $("#emess").remove();
+    } else if (school === "lawrence") {
+        $("#stylesheet1").attr("href", "./assets/stylesheets/kansas.css");
+        var tLogo = $("<img>");
+        tLogo.attr("src", "assets/images/kansas_jayhawks.png");
+        tLogo.attr("alt", "University of Kansas Team Logo");
+        $("#teamLogo").html(tLogo);
+        $("#emess").remove();
+    } else if (school === "manhattan") {
+        $("#stylesheet1").attr("href", "./assets/stylesheets/ksu.css");
+        var tLogo = $("<img>");
+        tLogo.attr("src", "assets/images/kansas_state_wildcats.png");
+        tLogo.attr("alt", "Kansas State University Team Logo");
+        $("#teamLogo").html(tLogo);
+        $("#emess").remove();
+    } else if (school === "norman") {
+        $("#stylesheet1").attr("href", "./assets/stylesheets/oklahoma.css");
+        var tLogo = $("<img>");
+        tLogo.attr("src", "assets/images/oklahoma_sooners.png");
+        tLogo.attr("alt", "Oklahoma University Team Logo");
+        $("#teamLogo").html(tLogo);
+        $("#emess").remove();
+    } else if (school === "stillwater") {
+        $("#stylesheet1").attr("href", "./assets/stylesheets/osu.css");
+        var tLogo = $("<img>");
+        tLogo.attr("src", "assets/images/oklahoma_state_cowboys.png");
+        tLogo.attr("alt", "Oklahoma State University Team Logo");
+        $("#teamLogo").html(tLogo);
+        $("#emess").remove();
+    } else if (school === "fort worth") {
+        $("#stylesheet1").attr("href", "./assets/stylesheets/tcu.css");
+        var tLogo = $("<img>");
+        tLogo.attr("src", "assets/images/tcu_horned_frogs.png");
+        tLogo.attr("alt", "Texas Christian University Team Logo");
+        $("#teamLogo").html(tLogo);
+        $("#emess").remove();
+    } else if (school === "lubbock") {
+        $("#stylesheet1").attr("href", "./assets/stylesheets/ttu.css");
+        var tLogo = $("<img>");
+        tLogo.attr("src", "assets/images/texas_tech_red_raiders.png");
+        tLogo.attr("alt", "Texas Tech University Team Logo");
+        $("#teamLogo").html(tLogo);
+        $("#emess").remove();
+    } else if (school === "morgantown") {
+        $("#stylesheet1").attr("href", "./assets/stylesheets/wvu.css");
+        var tLogo = $("<img>");
+        tLogo.attr("src", "assets/images/west_virginia_mountaineers.png");
+        tLogo.attr("alt", "West Virginia University Team Logo");
+        tLogo.attr("id", "teamPicture")
+        $("#teamLogo").html(tLogo);
+        $("#emess").remove();
+    } else { 
+        $("#stylesheet1").attr("href", "style.css");
+        $(".card").css("display", "none");
+        eMessageBox = $("<div>");
+        eMessageBox.attr("id", "emess")
+        eMessageBox.addClass("container");
+        eMessage = $("<h1>");
+        eMessage.html("I'm sorry there is no BIG12 school in " + q + " ,please try your search again.");
+        $("#teamLocals").show();
+        $(eMessageBox).append(eMessage);
+        $("#teamLocals").append(eMessageBox);
+        $("#teamPicture").remove();
+    };
+    
 })
 
 function weatherReturn(q) {
@@ -28,6 +115,7 @@ function weatherReturn(q) {
         var eMessageBox;
         var eMessage;
         var weatherIcon = $("<img>");
+        weatherIcon.attr("id", "weatherPic")
         weatherIcon.attr("src", "http://openweathermap.org/img/w/" + response.weather[0].icon + ".png")
 
         $("#weatherCity").text(response.name);
@@ -35,101 +123,7 @@ function weatherReturn(q) {
         $("#weatherIcon").html(weatherIcon);
         $("#weatherCondition").text(response.weather[0].description);
 
-        if (q === "austin") {
-            $("body").css("background-color", "#CC5500");
-            $("body").css("color", "white");
-            $("nav").css("background-color", "white");
-            $("nav").css("color", "#CC5500");
-            $(".card-header").css("background-color", "white");
-            $(".card-header").css("color", "#CC5500");
-            $("#emess").remove();
-        } else if (q === "waco") {
-            $("body").css("background-color", "#002419");
-            $("body").css("color", "#fdba31");
-            $("nav").css("background-color", "#fdba31");
-            $("nav").css("color", "#002419");
-            $(".card-header").css("background-color", "#fdba31");
-            $(".card-header").css("color", "#002419");
-            $("#emess").remove();
-        } else if (q === "ames") {
-            $("body").css("background-color", "#C8102E");
-            $("body").css("color", "#F1BE48");
-            var tLogo = $("<img>");
-            tLogo.attr("src", "assets/images/baylor_bears.png");
-            tLogo.attr("alt", "University of Baylor Team Logo");
-            $("#teamLogo").html(tLogo);
-            $("nav").css("background-color", "#F1BE48");
-            $("nav").css("color", "#C8102E");
-            $(".card-header").css("background-color", "#F1BE48");
-            $(".card-header").css("color", "#C8102E");
-            $("#emess").remove();
-        } else if (q === "lawrence") {
-            $("body").css("background-color", "#0051BA");
-            $("body").css("color", "#E8000D");
-            $("nav").css("background-color", "#E8000D");
-            $("nav").css("color", "#0051BA");
-            $(".card-header").css("background-color", "#E8000D");
-            $(".card-header").css("color", "#0051BA");
-            $("#emess").remove();
-        } else if (q === "manhattan") {
-            $("body").css("background-color", "#512888");
-            $("body").css("color", "#A7A7A7");
-            $("nav").css("background-color", "#A7A7A7");
-            $("nav").css("color", "#512888");
-            $(".card-header").css("background-color", "#A7A7A7");
-            $(".card-header").css("color", "#512888");
-            $("#emess").remove();
-        } else if (q === "norman") {
-            $("body").css("background-color", "#841719");
-            $("body").css("color", "#fcfada");
-            $("nav").css("background-color", "#fcfada");
-            $("nav").css("color", "#841719");
-            $(".card-header").css("background-color", "#fcfada");
-            $(".card-header").css("color", "#841719");
-            $("#emess").remove();
-        } else if (q === "stillwater") {
-            $("body").css("background-color", "#FF7300");
-            $("body").css("color", "#000000");
-            $("nav").css("background-color", "#000000");
-            $("nav").css("color", "#FF7300");
-            $(".card-header").css("background-color", "#000000");
-            $(".card-header").css("color", "#FF7300");
-            $("#emess").remove();
-        } else if (q === "fort worth") {
-            $("body").css("background-color", "#4D1979");
-            $("body").css("color", "#FFFFFF");
-            $("nav").css("background-color", "#FFFFFF");
-            $("nav").css("color", "#4D1979");
-            $(".card-header").css("background-color", "#FFFFFF");
-            $(".card-header").css("color", "#4D1979");
-            $("#emess").remove();
-        } else if (q === "lubbock") {
-            $("body").css("background-color", "#CC0000");
-            $("body").css("color", "#000000");
-            $("nav").css("background-color", "#000000");
-            $("nav").css("color", "#CC0000");
-            $(".card-header").css("background-color", "#000000");
-            $(".card-header").css("color", "#CC0000");
-            $("#emess").remove();
-        } else if (q === "morgantown") {
-            $("body").css("background-color", "#002855");
-            $("body").css("color", "#EAAA00");
-            $("nav").css("background-color", "#EAAA00");
-            $("nav").css("color", "#002855");
-            $(".card-header").css("background-color", "#EAAA00");
-            $(".card-header").css("color", "#002855");
-            $("#emess").remove();
-        } else { 
-            $(".card").css("display","none");
-            eMessageBox = $("<div>");
-            eMessageBox.attr("id", "emess")
-            eMessageBox.addClass("container");
-            eMessage = $("<h1>");
-            eMessage.html("I'm sorry there is no BIG12 school in " + q + " ,please try your search again.");
-            $("#teamLocals").show();
-            $(eMessageBox).append(eMessage);
-            $("#teamLocals").append(eMessageBox);
-        };
+        
     });
 }
 
